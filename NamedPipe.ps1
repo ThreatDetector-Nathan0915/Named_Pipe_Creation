dbd3e8d45340639cd2dd07cb35eb5a7f21bb8725
ThreatDetector-Nathan0915 <hanephinn@gmail.com>
2024-07-10 10:30:38 -0500

    Update NamedPipe.ps1
    Better Error Logging

--- NamedPipe.ps1
+++ NamedPipe.ps1
@@ -8,6 +8,7 @@ param (
 $pipeName = "\\.\pipe\TDE_TEST_PIPE"
 
 function Create-NamedPipe {
+    Write-Host "Creating named pipe: $pipeName"
     $pipe = New-Object System.IO.Pipes.NamedPipeServerStream(
         "TDE_TEST_PIPE", 
         [System.IO.Pipes.PipeDirection]::InOut, 
@@ -17,8 +18,9 @@ function Create-NamedPipe {
         4096, 
         4096
     )
-
+    Write-Host "Waiting for client connection..."
     $pipe.WaitForConnection()
+    Write-Host "Client connected."
     return $pipe
 }
 
@@ -27,14 +29,18 @@ function Write-ToPipe {
         [string]$message
     )
 
+    Write-Host "Connecting to named pipe: $pipeName"
     $pipeClient = New-Object System.IO.Pipes.NamedPipeClientStream(".", "TDE_TEST_PIPE", [System.IO.Pipes.PipeDirection]::Out)
     $pipeClient.Connect()
+    Write-Host "Connected to named pipe."
 
     $streamWriter = New-Object System.IO.StreamWriter($pipeClient)
     $streamWriter.AutoFlush = $true
+    Write-Host "Writing message to pipe: $message"
     $streamWriter.WriteLine($message)
     $streamWriter.Close()
     $pipeClient.Close()
+    Write-Host "Message written and pipe closed."
 }
 
 function Read-FromPipe {
@@ -51,6 +57,7 @@ function Read-FromPipe {
     }
     $streamReader.Close()
     $pipe.Close()
+    Write-Host "Pipe closed."
 }
 
 if ($mode -eq "read") {
