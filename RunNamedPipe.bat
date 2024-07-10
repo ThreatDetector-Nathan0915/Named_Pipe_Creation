3baedace6aa1d854572fe61b83b6927959eeb733
ThreatDetector-Nathan0915 <hanephinn@gmail.com>
2024-07-10 11:10:14 -0500

    Update RunNamedPipe.bat

--- RunNamedPipe.bat
+++ RunNamedPipe.bat
@@ -4,9 +4,6 @@ setlocal
 REM Path to the PowerShell script
 set scriptPath=%~dp0NamedPipe.ps1
 
-REM Message to send through the pipe
-set message="Your message here"
-
 REM Start the PowerShell script to read from the pipe in a new window
 start powershell -NoExit -ExecutionPolicy Bypass -Command "& '%scriptPath%' -mode read"
 
@@ -14,6 +11,6 @@ REM Wait a moment to ensure the read script is ready
 timeout /t 2 /nobreak > nul
 
 REM Start the PowerShell script to write to the pipe in a new window
-powershell -NoExit -ExecutionPolicy Bypass -Command "& '%scriptPath%' -mode write -message %message%"
+start powershell -NoExit -ExecutionPolicy Bypass -Command "& '%scriptPath%' -mode write"
 
 endlocal
